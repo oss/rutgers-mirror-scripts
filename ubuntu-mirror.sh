2cba8bfffe60412710251fec678da57e49fbeedc
Kyle Suarez <kds124@nbcs.rutgers.edu>
2013-08-26 11:33:34 -0400

    Trying to clean up the Ubuntu mirror script

--- ubuntu-mirror.sh
+++ ubuntu-mirror.sh
@@ -1,67 +1,75 @@
 #!/bin/bash
-## Mirror Synchronization Script /usr/local/bin/ubuntu-mirror-sync.sh
-## Version 1.01 Updated 13 Feb 2007 by Peter Noble
 
-## Where do we want to have our files
+# Mirror Synchronization Script /army/rpmprivate/centos/config/scripts/cron
+# Version 1.01 Updated 13 Feb 2007 by Peter Noble
+# Updated for Rutgers, based on the old mirror script on Mon Aug 5 2013,
+#   with some other small fixes and improvements for clarity's sake
+#
+# Troubleshooting (in case omachi dies again):
+# - Make sure limbus is properly mounted.
+# - Ensure that the crontab includes all of the mirror scripts.
+# - Make sure a symbolic link at / exists and points properly to the mirrors
+#   with `cd /; ln -s limbus/centos/ mirror`
+
+# The place we want to keep our files
 SYNC_HOME="/limbus/centos/ubuntu"
-log="/limbus/centos/logs/ubuntu_sync.log"
-UPDATE_FILE=/limbus/centos/status/UBUNTU.LAST_UPDATE
+LOG="limbus/centos/logs/ubuntu_sync.log"
+UPDATE_FILE="/limbus/centos/status/UBUNTU.LAST_UPDATE"
 
-
-#email to report on out of date
+# Some email settings
 EMAIL="oss@oss.rutgers.edu"
-## Setup the server to mirror
-#remote=rsync://archive.ubuntu.com/ubuntu
-remote=rsync://mirrors.rit.edu/ubuntu
+EMAIL_SUBJECT="UBUNTU MIRROR: 24 Hours Out-of-Date"
+EMAIL_BODY="Check logs at http://centos.rutgers.edu/mirror/status"
 
+# Set up the server to mirror
+#REMOTE="rsync://archive.ubuntu.com/ubuntu"
+#REMOTE="rsync://mirror.anl.gov/ubuntu/"
+REMOTE="rsync://rsync.gtlib.gatech.edu/ubuntu/"
 
-## Setup the local directory / Our mirror
-local=$SYNC_HOME
+# Set up the local directory / Our mirror
+LOCAL="$SYNC_HOME"
 
-## Initialize some other variables
-complete="false"
-failures=0
-status=1
-pid=$$
+# For the rsync command
+RSYNC_FLAGS="--recursive --times --links --hard-links --stats --verbose"
+EXCLUDE_LIST='--exclude "Packages*" --exclude "Sources*" --exclude "Release*" --exclude .*~tmp~*'
 
-echo "`date +%x-%R` - $pid - Started Ubuntu Mirror Sync" > $log
-while [[ "$complete" != "true" ]]; do
+# Initialize some other variables
+STATUS=1
+PID=$$
 
-        if [[ $1 == "debug" ]]; then
-                echo "Working on attempt number $failures"
-		rsync --progress --recursive --times --links --hard-links --stats --exclude "Packages*" --exclude "Sources*" --exclude "Release*" $remote $local 2>&1 >> $log && \
-		rsync --progress --recursive --times --links --hard-links --stats --delete --delete-after $remote $local 2>&1 >> $log
-                status=$?
-        else
-		rsync --progress --recursive --times --links --hard-links --stats --exclude "Packages*" --exclude "Sources*" --exclude "Release*" $remote $local  2>&1 >> $log && \
-		rsync --progress --recursive --times --links --hard-links --stats --delete --delete-after $remote $local 2>&1 >> $log
-                status=$?
+# Begin logging.
+echo "`date +%x-%R` - $PID - Started Ubuntu Mirror Sync" >> $LOG
+if [[ $1 == "debug" ]]; then
+        echo "ubuntu-mirror.sh: [DEBUG] Beginning the sync."
+fi
+
+# Do the actual syncing.
+# There used to be a loop here, but we only want to attempt the sync once.
+rsync $RSYNC_FLAGS $EXCLUDE_LIST $REMOTE $LOCAL 2>&1 >> $LOG && \
+rsync $RSYNC_FLAGS --delete --delete-after $REMOTE $LOCAL 2>&1 >> $LOG
+STATUS=$?
+
+# Now check to see how our sync worked.
+if [[ $STATUS -ne "0" ]]; then
+        # Write to the syslog.
+        logger -p cron.err -t $0 `tail -n 5 $log`
+
+        # If the rsync fails, email us.
+        OUTOFDATE=`find /mirror/status -mtime -1 -name 'UBUNTU*' | wc -l`                                                                                                                                                        
+        if [[ "$OUTOFDATE" -eq 0 ]]; then
+                echo "ubuntu-mirror.sh: [FAIL] Sync failed. Ubuntu mirror out of date."
+                echo $EMAIL_BODY | mail -s $EMAIL_SUBJECT $EMAIL
         fi
+        exit 1
+else
+        # First, gather some data
+        DATENICE=`date \`\`+%a\ -\ %b\ %d,\ %Y\ -\ %H:%M''`
+        LATEST_REPO_FILE=`ls -t $(find $SYNC_HOME/dists/ -name Packages.gz -print0 | xargs -0 echo) | head -1`
+        REPODATA_DIR_DATE=`date -r $LATEST_REPO_FILE \`\`+%a\ -\ %b\ %d,\ %Y\ -\ %H:%M''`
 
-        if [[ $status -ne "0" ]]; then
-                complete="true"
-                (( failures += 1 ))
-	        #if our rsync failed, log it and email us
-
-		logger -p cron.err -t $0 `tail -n 5 $log` #write log to syslog on failure
-		# Mon Aug 5 2013: This used to search for "ARCHLINUX" instead of "UBUNTU"
-		OUTOFDATE=`find /mirror/status -mtime -1 -name 'UBUNTU*' | wc -l`
-		if [ "$OUTOFDATE" -eq 0 ]
-		then
-			echo "sync out of date\n";
-		#	echo "Warning: Ubuntu mirror out of date by 24 hours, please check logs at http://centos.rutgers.edu/mirror/status " | mail -s "UBUNTU MIRROR 24 Hours out of date" $EMAIL
-		fi
-
-        else
-                echo "`date +%x-%R` - $pid - Finished Ubuntu Mirror Sync" >> $log
-		DATENICE=`date \`\`+%a\ -\ %b\ %d,\ %Y\ -\ %H:%M''`
-		 echo $DATENICE > $UPDATE_FILE
-		#LATEST_REPO_FILE=`find $SYNC_HOME/dists/ -name Packages.gz -print0 | xargs -r -0 ls -t | head -1`
-		LATEST_REPO_FILE=`ls -t $(find $SYNC_HOME/dists/ -name Packages.gz -print0 | xargs -0 echo) | head -1`
-		REPODATA_DIR_DATE=`date -r $LATEST_REPO_FILE \`\`+%a\ -\ %b\ %d,\ %Y\ -\ %H:%M''`
-		echo $REPODATA_DIR_DATE >> $UPDATE_FILE
-        complete="true"
-        fi
-done
-
-exit 0
+        # Update the mirror status
+        echo "`date +%x-%R` - $PID - Finished Ubuntu Mirror Sync" >> $LOG
+        echo $DATENICE > $UPDATE_FILE
+        echo $REPODATA_DIR_DATE >> $UPDATE_FILE
+        exit 0
+fi
